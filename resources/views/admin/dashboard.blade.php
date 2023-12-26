fe5653546904daabe1351482d26535a6adbeb1d6
Katarina Inezita <nicholausadhyatma@gmail.com>
2023-12-26 12:22:14 +0700

    table

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,14 +1,69 @@
 <x-layout>
 
 
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      @foreach ($users as $user)
-          <a href="{{ route('admin.detail', ['id' => $user->id]) }}"
-              class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-  
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $user->username }}</h5>
-              <p class="font-normal text-gray-700 dark:text-gray-400">{{ $user->nama_lengkap }}</p>
-          </a>
-      @endforeach
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <caption
+                class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                Daftar Pengguna
+            </caption>
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        No
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nama
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Username
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Usia
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Pendidikan
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Tes Result</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+
+
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $loop->iteration }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $user->nama_lengkap }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->username }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->usia }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->pendidikan_terakhir }}
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="{{ route('admin.detail', ['id' => $user->id]) }}"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
     </div>
+
+
+
+
+
 </x-layout>
